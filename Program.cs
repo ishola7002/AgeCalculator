7bc3771eb2aea631cb32b84dd36621eb77424cb8
Zafrullah Giwa <zafrullahgiwa@gmail.com>
2024-12-28 09:23:36 +0100

    Age calcalation

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ENter your age");
+int age = Convert.ToInt32(Console.ReadLine());
+if(age <= 15)
+{
+  Console.WriteLine("Not adult");
+}
+else
+{
+  Console.WriteLine("Adult");
+}
